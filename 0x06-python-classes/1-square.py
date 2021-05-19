f662c970e768510662a3bad6479a42ff103491f5
jdcera4 <jdcera4@gmail.com>
2021-05-18 20:41:50 -0500

    initialize

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defines a class called Square"""
+
+
+class Square:
+
+    """Represents a square"""
+
+    def __init__(self, size):
+
+        """Initializes the data"""
+        
+
+        self.__size = size
+        
