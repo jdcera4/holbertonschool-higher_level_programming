285740de5374b6fcc01129ef68314685374a6a03
jdcera4 <jdcera4@gmail.com>
2021-05-20 19:33:22 -0500

    crrection

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,22 +21,22 @@ class Rectangle:
             if value < 0:
                 raise ValueError("width must be >= 0")
             else:
-                self.__value = value
+                self.__width = value
         else:
             raise TypeError("width must be an integer")
     
     @property
-    def width(self):
+    def height(self):
         """Initializes the data"""
         return self.__height
     
-    @width.setter
-    def width(self, value):
+    @height.setter
+    def height(self, value):
         """setter of width, for create the conditionals"""
         if type(value) is int:
             if value < 0:
                 raise ValueError("width must be >= 0")
             else:
-                self.__value = value
+                self.__height = value
         else:
             raise TypeError("width must be an integer")
