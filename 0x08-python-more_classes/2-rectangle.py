d0fe5feca5a8f8c17a00a913fa06781385fd4f8f
jdcera4 <jdcera4@gmail.com>
2021-05-20 20:07:05 -0500

    if in paremeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,4 +47,6 @@ class Rectangle:
 
     def perimeter(self):
         """Calculation perimeter"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
         return (self.__width * 2) + (self.__height * 2)
