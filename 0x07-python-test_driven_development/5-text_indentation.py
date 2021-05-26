9a30dbd96889284cd6cab98720e33bd59fd5e6bd
jdcera4 <jdcera4@gmail.com>
2021-05-25 22:06:36 -0500

    print text identation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
+"""script print text"""
 
 def text_indentation(text):
-    """function"""
+    """function print text"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
 
     for i in text:
+
         if i == '.' or i == '?' or i == ':':
             print("{}\n".format(i))
         else:
