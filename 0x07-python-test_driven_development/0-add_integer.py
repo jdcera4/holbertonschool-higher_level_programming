a7082ed72bbdc76fb09d47eb17610fd404e1c9ef
jdcera4 <jdcera4@gmail.com>
2021-05-25 21:30:50 -0500

    add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@
 
 
 def add_integer(a, b=98):
-    """Add function"""
+    """Function add two integers"""
     if not isinstance(a, (int, float)) or a is None:
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
