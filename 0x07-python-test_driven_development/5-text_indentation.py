61d1c236f104cc24f0b69bcaf82feb81e5a23e1e
jdcera4 <jdcera4@gmail.com>
2021-05-25 22:37:11 -0500

    print text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,7 +7,6 @@ def text_indentation(text):
         raise TypeError("text must be a string")
 
     for i in text:
-
         if i == '.' or i == '?' or i == ':':
             print("{}\n".format(i))
         else:
