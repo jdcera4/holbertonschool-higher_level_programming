b6939aae3854e58ee2258fe8b9c652acab4e05ef
jdcera4 <jdcera4@gmail.com>
2021-05-25 22:23:57 -0500

    print #

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-
+"""script of python 
+print #"""
 
 def print_square(size):
     """Functioon print # """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
     if size == 0:
         print("")
     for row in range(size):
