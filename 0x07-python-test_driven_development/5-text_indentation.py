1a62e3bdec11f281297a4a35436f16c501601aa6
jdcera4 <jdcera4@gmail.com>
2021-05-26 09:20:43 -0500

    line

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,11 +3,18 @@
 
 def text_indentation(text):
     """function print text"""
+    flag = 0
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
     for i in text:
-        if i == '.' or i == '?' or i == ':':
+        if flag == 1 and i is ' ':
+            print('', end='')
+            flag = 0
+            continue
+        if i is '.' or i is '?' or i is ':':
             print("{}\n".format(i))
+            flag = 1
         else:
             print(i, end='')
+            flag = 0
