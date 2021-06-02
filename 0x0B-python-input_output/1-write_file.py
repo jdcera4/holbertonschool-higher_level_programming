ce036d374f176742a8b5af6186c1ea389e3b2c2f
jdcera4 <jdcera4@gmail.com>
2021-06-02 14:25:04 -0500

    write function and len

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that writes a string to a text file (UTF8)
+and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """Print text in the filename"""
+    with open(filename,"w", encoding="utf-8") as f:
+        cont = f.write(text)
+        return cont
