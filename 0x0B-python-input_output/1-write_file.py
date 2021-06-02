7f179dc4d72b20bb0088342a8104fc73e779ae59
jdcera4 <jdcera4@gmail.com>
2021-06-02 14:28:14 -0500

    pep8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,6 @@ and returns the number of characters written
 
 def write_file(filename="", text=""):
     """Print text in the filename"""
-    with open(filename,"w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         cont = f.write(text)
         return cont
