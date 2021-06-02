bbf694a7f3b6aebdd9f4d47505be16de03394513
jdcera4 <jdcera4@gmail.com>
2021-06-02 17:05:59 -0500

    pep8

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""function that appends a string at the end of a text file (UTF8) and returns 
+"""function that appends a string at the end of a text file (UTF8) and returns
 the number of characters added:
 """
+
 
 def append_write(filename="", text=""):
     """Print text in the filename"""
