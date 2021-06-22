5965cdc2cf2c6e292e91381ae59176ba870f8129
jdcera4 <jdcera4@gmail.com>
2021-06-22 15:38:50 -0500

    pep 8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,19 +2,21 @@
 """
 Print roman numbers
 """
+
+
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
     else:
-        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                 'D': 500, 'M': 1000}
         i = 0
         num = 0
         while i < len(roman_string):
-            if i+1<len(roman_string) and roman_string[i:i+2] in roman:
-                num+=roman[roman_string[i:i+2]]
-                i+=2
+            if i + 1 < len(roman_string) and roman_string[i: i + 2] in roman:
+                num += roman[roman_string[i:i+2]]
+                i += 2
             else:
-                #print(i)
-                num+=roman[roman_string[i]]
-                i+=1
+                num += roman[roman_string[i]]
+                i += 1
         return num
