ddead05275a4954a64fa7e472b683ed643e7f245
jdcera4 <jdcera4@gmail.com>
2021-06-22 15:47:14 -0500

    pep8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,7 +9,7 @@ def roman_to_int(roman_string):
         return 0
     else:
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-                 'D': 500, 'M': 1000}
+                 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90}
         i = 0
         num = 0
         while i < len(roman_string):
