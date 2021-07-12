f43bb010eb41891a20b1780ee17a3f41a20d5d4d
jdcera4 <jdcera4@gmail.com>
2021-07-12 16:22:30 -0500

    coment

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
--- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- lists all records of the table second_table of the database hbtn_0c_0 in
+-- your MySQL server
 SELECT score, name from second_table WHERE name is not null order by score desc;
