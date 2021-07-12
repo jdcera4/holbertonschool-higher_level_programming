93262cfacee28f4862e0c425d8ecb4c89391abf4
jdcera4 <jdcera4@gmail.com>
2021-07-12 15:52:40 -0500

    error

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,4 @@
 -- displays the number of records with id = 89 in the table
 -- first_table of the database
-select count(*) from first_table where id = 89;
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
