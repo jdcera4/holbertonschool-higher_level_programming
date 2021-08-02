04f4aca10a10278f6ff7994d1a70525900c7cd1c
jdcera4 <jdcera4@gmail.com>
2021-08-02 16:32:39 -0400

    spaces

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 // prints x times “C is fun”
 const text = 'C is fun'
 if (isNaN(process.argv[2])) {
-    console.log();
+  console.log();
 } else {
-    for (let cont = 0; cont < parseInt(process.argv[2]); cont++) {
-        console.log(text);
-    }
+  for (let cont = 0; cont < parseInt(process.argv[2]); cont++) {
+    console.log(text);
+  }
 }
