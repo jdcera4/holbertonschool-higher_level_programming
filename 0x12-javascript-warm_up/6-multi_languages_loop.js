ae7e791ae2093200d122dc3c178f55e0952c417f
jdcera4 <jdcera4@gmail.com>
2021-08-02 12:16:30 -0400

    prints 3 lines but by using an array of stringloop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// prints 3 lines but by using an array of string and a loop
+const ar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++){
+  console.log(ar[i]);
+}
