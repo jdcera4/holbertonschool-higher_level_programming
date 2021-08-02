aadf27d6470f22fc24b0964e9bd87d5a0184379c
jdcera4 <jdcera4@gmail.com>
2021-08-02 16:38:06 -0400

    texts

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // prints 3 lines but by using an array of string and a loop
-const ar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const ar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(ar[i]);
 }
