f1c5da3ea37d91dc444ecff1a0fe821df8c3c023
jdcera4 <jdcera4@gmail.com>
2021-08-03 12:09:20 -0400

    print character

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+    constructor(size) {
+        super(size, size);
+    }
+    charPrint(c) {
+        if (!c) {
+            this.print();
+        } else {
+            for (let cont = 0; cont < this.height; cont++) {
+                console.log(c.repeat(this.width));
+            }
+        }
+    }
+}
+module.exports = Square;
