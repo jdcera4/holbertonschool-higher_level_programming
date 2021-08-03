350f19ad19659e16441caf462edc686859c97e88
jdcera4 <jdcera4@gmail.com>
2021-08-03 17:45:17 -0400

    semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const SquareBase = require('./5-square');
 class Square extends SquareBase {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X'
+      c = 'X';
     }
     for (let cont = 0; cont < this.height; cont++) {
       console.log(c.repeat(this.width));
