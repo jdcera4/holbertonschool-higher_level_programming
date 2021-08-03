415f66e0eaaf1f8ed24dfbf8fda8a0591acf7d12
jdcera4 <jdcera4@gmail.com>
2021-08-03 11:27:22 -0400

    create method print() and prints character X

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+      if (h > 0 && w > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+    print () {
+        for (let cont = 0; cont > this.height; cont++) {
+            console.log('X'.repeat(this.width));
+        }
+    }
+  }
+  module.exports = Rectangle;
+  
