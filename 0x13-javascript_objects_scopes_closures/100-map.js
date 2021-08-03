bb7caca955be7712e175619c98f7acde7367577f
jdcera4 <jdcera4@gmail.com>
2021-08-03 18:04:06 -0400

    semistandard

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,6 +4,6 @@
 const originalList = require('./100-data').list;
 console.log(originalList);
 const mappedList = originalList.map(function (e, index) {
-    return (e * index);
+  return (e * index);
 });
 console.log(mappedList);
