1cb875cde552dfa0a2ae0efc10a62f3d8b1ec339
jdcera4 <jdcera4@gmail.com>
2021-08-03 16:00:37 -0400

    print list in reverse

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+    const reverse_list = [];
+    const last = list.length - 1;
+
+    for (let increment = last; increment >= 0; increment--) {
+        reverse_list.push(list[increment]);
+    }
+    return (reverse_list);
+}
