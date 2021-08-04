6b2fe4b1e1a521ab14a4a46d48645c51d9faf28d
jdcera4 <jdcera4@gmail.com>
2021-08-04 10:12:12 -0400

    add function in sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
+  const list = process.argv.sort(function (a, b) { return (a - b)});
   console.log(list.reverse()[1]);
 }
