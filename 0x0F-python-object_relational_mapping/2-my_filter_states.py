dea957510dfe87daf80c46df3699d563c2988ccc
jdcera4 <jdcera4@gmail.com>
2021-08-05 15:18:05 -0400

    add comment

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
+"""
+python script that lists all states from the database hbtn_0e_0_usa with a
+given name and is safe from MySQL injections
+"""
 import MySQLdb
 from sys import argv
 
 argument = argv[4]
-print(argv)
 conn = MySQLdb.connect(host="localhost", port=3306, user="root",
                             passwd="root", db="hbtn_0e_0_usa", charset="utf8")
 cur = conn.cursor()
