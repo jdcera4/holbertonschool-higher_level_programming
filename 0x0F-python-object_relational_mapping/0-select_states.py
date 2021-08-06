b55c2d18e3fb9d1c1512f34c04c83b75347bbb45
jdcera4 <jdcera4@gmail.com>
2021-08-06 10:44:25 -0400

    add arguments argv

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 import MySQLdb
+from sys import argv
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root",
-                            passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                            passwd=argv[2], db=argv[3], charset="utf8")
 cur = conn.cursor()
 # HERE I have to know SQL to grab all states in my database
 cur.execute("SELECT * FROM states ORDER BY id ASC")
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,12 +8,12 @@ from sys import argv
 
 if __name__ == "__main__":
     argument = argv[4]
-    conn = MySQLdb.connect(host="localhost", port=3306, user="root",
-                                passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
 
     # HERE I have to know SQL to grab all states in my database
-    cur.execute("SELECT * FROM states where name = %s ORDER BY id ASC;",(argument, ))
+    cur.execute("SELECT * FROM states where name LIKE %s ORDER BY id ASC;",(argument, ))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
