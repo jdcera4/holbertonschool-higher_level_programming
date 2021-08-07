12ca4918eb0436a27738189e9c50d9f17b77f625
jdcera4 <jdcera4@gmail.com>
2021-08-06 20:15:12 -0400

    add if __name__

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,16 +6,17 @@ given name and is safe from MySQL injections
 import MySQLdb
 from sys import argv
 
-argument = argv[4]
-conn = MySQLdb.connect(host="localhost", port=3306, user="root",
-                            passwd="root", db="hbtn_0e_0_usa", charset="utf8")
-cur = conn.cursor()
+if __name__ == "__main__":
+    argument = argv[4]
+    conn = MySQLdb.connect(host="localhost", port=3306, user="root",
+                                passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+    cur = conn.cursor()
 
-# HERE I have to know SQL to grab all states in my database
-cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
-    id ASC".format(argument))
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+        id ASC".format(argument))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
