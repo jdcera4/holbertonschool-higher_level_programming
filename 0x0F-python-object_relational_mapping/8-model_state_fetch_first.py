2235667946b01e83155ea22f3b5166f5bfda6c24
jdcera4 <jdcera4@gmail.com>
2021-08-09 09:33:49 -0400

    delete order by

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    first_state = session.query(State).order_by(State.id).first()
+    first_state = session.query(State).first()
     if first_state is not None:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
