b190477808a59567c89eafcbc669da2b958f9760
jdcera4 <jdcera4@gmail.com>
2021-08-20 16:40:17 -0400

    api request git hub

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""api request git hub"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    """apidata"""
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
+                                                              sys.argv[1])
+    result = requests.get(url)
+    d = result.json()
+    try:
+        
+        for i in range(10):
+            print("{}: {}".format(d[i]["sha"],
+                                  d[i]["commit"]["author"]["name"]))
+    except IndexError:
+        pass
