08107161bc831cc2cc77fa3202a8d78fc00600ea
jdcera4 <jdcera4@gmail.com>
2021-09-14 14:36:52 -0400

    semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-url = process.argv[2]
+const url = process.argv[2];
 request(url, function (error, response, body) {
   if (error) throw error;
   let con = 0;
