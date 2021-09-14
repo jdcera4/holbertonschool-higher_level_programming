616129af997630b8dceabf1839feafc1d6db3552
jdcera4 <jdcera4@gmail.com>
2021-09-14 16:31:07 -0400

    semistandard

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,20 +2,20 @@
 const request = require('request');
 const url = 'http://swapi-api.hbtn.io/api/films/' + parseInt(process.argv[2]);
 const start = function () {
-    request(url, function (error, response, body) {
-      if (error) throw error;
-      end(JSON.parse(body).characters, 0);
-    });
-  };
+  request(url, function (error, response, body) {
+    if (error) throw error;
+    end(JSON.parse(body).characters, 0);
+  });
+};
 
 const end = function (characters, i) {
-    if (characters.length === i) {
-      return;
-    }
-    request(characters[i], function (error, response, body) {
-      if (error) throw error;
-      console.log(JSON.parse(body).name);
-      end(characters, ++i);
-    });
-  };
+  if (characters.length === i) {
+    return;
+  }
+  request(characters[i], function (error, response, body) {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    end(characters, ++i);
+  });
+};
 start();
