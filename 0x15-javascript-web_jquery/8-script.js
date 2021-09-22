ca13cc641465cf742ec5bae5d72dfac9b5b4b8c2
jdcera4 <jdcera4@gmail.com>
2021-09-22 14:19:39 -0400

    fetches and lists the title

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(function () {
+    $.get('https://swapi-api.hbtn.io/api/films/?format=json',
+      function (data) {
+        $.each(data.results, function (i, movie) {
+          $('#list_movies').append($('<li></li>').text(movie.title));
+        });
+      });
+  });
+});
